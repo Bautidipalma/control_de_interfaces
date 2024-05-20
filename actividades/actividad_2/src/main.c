a9f23c9524803d97712cad8aca038cf0c4c02482
Bautidipalma <163869338+Bautidipalma@users.noreply.github.com>
2024-05-20 10:58:15 -0300

    Update main.c

--- actividades/actividad_2/src/main.c
+++ actividades/actividad_2/src/main.c
@@ -18,7 +18,14 @@ int main(void) {
 	printf("Un double ocupa %d bytes\n", sizeof(double));
 
     while (true) {
-
+	int16_t num1;  
+	int16_t num2;  
+	printf("ingrese un numero: ");
+	scanf("%hd", &num1);
+	fflush(stdin);
+	printf("ingrese otro numero: ");
+	scanf("%hd", &num2);
+	printf("el resultado es igual a %hd ", num1 + num2);
     }
     return 0;
 }
